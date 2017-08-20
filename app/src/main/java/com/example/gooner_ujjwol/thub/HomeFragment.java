5d402b54f0ea6d6e9bae4d8d6dcc1e3744127ce8
GunnerUjjwol <gunner04ujjwol@gmail.com>
2017-08-21 02:10:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gooner_ujjwol/thub/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.gooner_ujjwol.thub;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gooner-Ujjwol on 8/20/2017.
+ */
+
+public class HomeFragment extends Fragment {
+
+    View myView;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Home");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.home_layout,container,false);
+
+        return myView;
+    }
+}
---
+++ app/src/main/java/com/example/gooner_ujjwol/thub/ProfileFragment.java
@@ -0,0 +1,32 @@
+package com.example.gooner_ujjwol.thub;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gooner-Ujjwol on 8/20/2017.
+ */
+
+public class ProfileFragment extends Fragment {
+
+    View myView;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Profile");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.profile_layout,container,false);
+
+        return myView;
+    }
+}
